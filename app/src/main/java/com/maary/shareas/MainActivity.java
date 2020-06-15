e35743eb9b17848cedc12b8a168013f038927cdd
Steve Meng <dicedsteve@gmail.com>
2020-06-15 11:32:16 +0800

    修改文字提示为 Circle Progress Bar，Main Activity 背景设置为透明。

--- app/src/main/java/com/maary/shareas/MainActivity.java
+++ app/src/main/java/com/maary/shareas/MainActivity.java
@@ -11,9 +11,12 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
