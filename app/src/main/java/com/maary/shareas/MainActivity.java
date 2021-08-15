70e4cf7406494388cc1a51be01147f2ccfb5d43c
Steve Meng <dicedsteve@gmail.com>
2021-08-15 19:08:31 +0800

    尝试优化

--- app/src/main/java/com/maary/shareas/MainActivity.java
+++ app/src/main/java/com/maary/shareas/MainActivity.java
@@ -1,23 +1,12 @@
 package com.maary.shareas;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.view.WindowCompat;
-
-import android.Manifest;
 import android.app.AlertDialog;
 import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
@@ -27,10 +16,7 @@ import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
-import android.provider.SyncStateContract;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,7 +32,12 @@ import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.ToggleButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.WindowCompat;
+
 import com.hoko.blur.HokoBlur;
+import com.hoko.blur.task.AsyncBlurTask;
 
 import java.io.IOException;
 
@@ -55,11 +46,14 @@ public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     Bitmap blur;
     Bitmap brightness;
+    Rect cord;
 
     Button button;
     Button buttonFull;
     ToggleButton blurButton;
     ToggleButton brightnessButton;
+
+    private PictureThread thread;
 
     private static final int READ_EXTERNAL_STORAGE = 100;
 
@@ -85,6 +79,18 @@ public class MainActivity extends AppCompatActivity {
                     Boolean isVertical = isVertical(device_height, device_width, bitmap);
                     int bitmap_full_width = bitmap.getWidth();
                     int bitmap_full_height = bitmap.getHeight();
+                    int desired_width;
+                    int desired_height;
+                    if (isVertical){
+                        desired_width = device_width;
+                        float scale = (float) device_width/bitmap_full_width;
+                        desired_height = (int)(scale * bitmap_full_height);
+                    }else{
+                        desired_height = device_height;
+                        float scale = (float)device_height/bitmap_full_height;
+                        desired_width = (int)(scale * bitmap_full_width);
+                    }
+                    bitmap = Bitmap.createScaledBitmap(bitmap, desired_width, desired_height, true);
 
                     final WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
                     //Horizontal Scroll View
@@ -163,7 +169,6 @@ public class MainActivity extends AppCompatActivity {
 
                     imageView.setImageBitmap(bitmap);
 
-                    //XmlPullParser parser = Resources.getSystem().getXml(R.xml.customSeekbar);
                     SeekBar blurSeekbar = new SeekBar(this);
                     RelativeLayout.LayoutParams blurSeekbarParams = new RelativeLayout.LayoutParams(
                             ViewGroup.LayoutParams.MATCH_PARENT,
@@ -187,8 +192,21 @@ public class MainActivity extends AppCompatActivity {
                     blurSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                         @Override
                         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                            blur = HokoBlur.with(getApplicationContext()).radius(progress).blur(bitmap);
-                            imageView.setImageBitmap(blur);
+
+                            HokoBlur.with(getApplicationContext())
+                                    .radius(progress)
+                                    .asyncBlur(bitmap, new AsyncBlurTask.Callback() {
+                                        @Override
+                                        public void onBlurSuccess(Bitmap bitmap) {
+                                            blur = bitmap;
+                                            imageView.setImageBitmap(bitmap);
+                                        }
+
+                                        @Override
+                                        public void onBlurFailed(Throwable error) {
+
+                                        }
+                                    });
                         }
 
                         @Override
@@ -200,6 +218,9 @@ public class MainActivity extends AppCompatActivity {
                         public void onStopTrackingTouch(SeekBar seekBar) {
                         }
                     });
+
+                    thread = new PictureThread(imageView, bitmap);
+                    thread.start();
 
                     SeekBar brightnessSeekbar = new SeekBar(this);
                     RelativeLayout.LayoutParams brightnessSeekbarParams = new RelativeLayout.LayoutParams(
@@ -223,8 +244,7 @@ public class MainActivity extends AppCompatActivity {
                     brightnessSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                         @Override
                         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                            brightness = adjustBrightness(bitmap, progress - 51);
-                            imageView.setImageBitmap(brightness);
+                            thread.adjustBrightness(progress-51);
                         }
 
                         @Override
@@ -237,6 +257,39 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
 
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Set AS");
+
+                    String[] options = {"home", "lockscreen", "home & lockscreen"};
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case 0:
+                                    try {
+                                        wallpaperManager.setBitmap(bitmap, cord, true, WallpaperManager.FLAG_SYSTEM);
+                                        break;
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                case 1:
+                                    try {
+                                        wallpaperManager.setBitmap(bitmap, cord, true, WallpaperManager.FLAG_LOCK);
+                                        break;
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                case 2:
+                                    try {
+                                        wallpaperManager.setBitmap(bitmap, cord, true, WallpaperManager.FLAG_SYSTEM|WallpaperManager.FLAG_LOCK);
+                                        break;
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                            }
+                        }
+                    });
+
                     //Set Button
                     button = new Button(this);
                     LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
@@ -246,68 +299,17 @@ public class MainActivity extends AppCompatActivity {
                     button.setText("SET");
                     button.setId(View.generateViewId());
                     button.setLayoutParams(buttonParams);
-                    //Bitmap finalBitmap = bitmap;
                     button.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Rect cord = new Rect(0, 0, 0, 0);
+
                             if (isVertical) {
-                                float scale = (float) bitmap_full_height / verticalScrollView.getChildAt(0).getHeight();
-
-                                int start = (int) (scale * verticalScrollView.getScrollY());
-
-                                int result_width = (int) (scale * device_width);
-                                int result_height = (int) (scale * device_height);
-                                result_width = Math.min(result_width, bitmap_full_width);
-                                result_height = Math.min(result_height, bitmap_full_height);
-
-                                cord = new Rect(0, start, result_width, start + result_height);
+                                int start = verticalScrollView.getScrollY();
+                                cord = new Rect(0, start, device_width, start + device_height);
                             } else {
-                                float scale = (float) bitmap_full_width / horizontalScrollView.getChildAt(0).getWidth();
-
-                                int start = (int) (scale * horizontalScrollView.getScrollX());
-                                int result_width = (int) (scale * device_width);
-                                int result_height = (int) (scale * device_height);
-                                result_width = Math.min(result_width, bitmap_full_width);
-                                result_height = Math.min(result_height, bitmap_full_height);
-
-                                cord = new Rect(start, 0, start + result_width, result_height);
+                                int start = horizontalScrollView.getScrollX();
+                                cord = new Rect(start, 0, start + device_width, device_height);
                             }
-
-                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                            builder.setTitle("Set AS");
-
-                            String[] options = {"home", "lockscreen", "home & lockscreen"};
-                            Rect finalCord = cord;
-                            builder.setItems(options, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    switch (which) {
-                                        case 0:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap, finalCord, true, WallpaperManager.FLAG_SYSTEM);
-                                                break;
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                        case 1:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap, finalCord, true, WallpaperManager.FLAG_LOCK);
-                                                break;
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                        case 2:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap, finalCord, true, WallpaperManager.FLAG_SYSTEM|WallpaperManager.FLAG_LOCK);
-                                                break;
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                    }
-                                }
-                            });
-
                             AlertDialog dialog = builder.create();
                             dialog.show();
 
@@ -354,7 +356,6 @@ public class MainActivity extends AppCompatActivity {
                     brightnessButton.setText("BRIGHTNESS");
                     brightnessButton.setTextOff("BRIGHTNESS");
                     brightnessButton.setTextOn("OK");
-                    //blurButton.setBackgroundColor(Color.TRANSPARENT);
                     brightnessButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                         @Override
                         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -366,7 +367,8 @@ public class MainActivity extends AppCompatActivity {
                                 blurButton.setClickable(false); blurButton.setPressed(true);
                             } else {
                                 brightnessButton.setText("BRIGHTNESS");
-                                bitmap = brightness;
+                                bitmap = thread.finalBitmap();
+                                thread.interrupt();
                                 relativeLayout.removeView(brightnessSeekbar);
                                 button.setClickable(true); button.setPressed(false);
                                 buttonFull.setClickable(true); buttonFull.setPressed(false);
@@ -385,39 +387,7 @@ public class MainActivity extends AppCompatActivity {
                     buttonFull.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                            builder.setTitle("Set As");
-
-                            String[] options = {"Home", "Lockscreen", "Home & Lockscreen"};
-                            builder.setItems(options, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    switch (which){
-                                        case 0:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap,null,true, WallpaperManager.FLAG_SYSTEM);
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                            break;
-                                        case 1:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap,null,true, WallpaperManager.FLAG_LOCK);
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                            break;
-                                        case 2:
-                                            try {
-                                                wallpaperManager.setBitmap(bitmap,null,true, WallpaperManager.FLAG_SYSTEM|WallpaperManager.FLAG_LOCK);
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                            break;
-                                    }
-                                }
-                            });
-
+                            cord = null;
                             AlertDialog dialog = builder.create();
                             dialog.show();
                         }
@@ -474,23 +444,6 @@ public class MainActivity extends AppCompatActivity {
         double bitmap_scale = (double) bitmap_full_height / bitmap_full_width;
 
         return device_scale < bitmap_scale;
-    }
-
-    public static Bitmap adjustBrightness(Bitmap mBitmap, float brightness) {
-        ColorMatrix cm = new ColorMatrix(new float[]
-                {
-                        1, 0, 0, 0, brightness,
-                        0, 1f, 0, 0, brightness,
-                        0, 0, 1f, 0, brightness,
-                        0, 0, 0, 1f, 0
-                });
-        Bitmap mEnhancedBitmap = Bitmap.createBitmap(mBitmap.getWidth(), mBitmap.getHeight(), mBitmap
-                .getConfig());
-        Canvas canvas = new Canvas(mEnhancedBitmap);
-        Paint paint = new Paint();
-        paint.setColorFilter(new ColorMatrixColorFilter(cm));
-        canvas.drawBitmap(mBitmap, 0, 0, paint);
-        return mEnhancedBitmap;
     }
 
     public static class ShareReceiver extends BroadcastReceiver{
