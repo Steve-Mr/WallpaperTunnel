22a10c8ab5cee7f2e1ddddd3a117424eed51a286
maary <dicedsteve@gmail.com>
2021-12-30 21:02:00 +0800

    Set up history activity

---
+++ app/src/main/java/com/maary/shareas/HistoryActivity.java
@@ -0,0 +1,14 @@
+package com.maary.shareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
