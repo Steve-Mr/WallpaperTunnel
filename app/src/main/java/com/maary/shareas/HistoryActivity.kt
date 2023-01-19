0f55c02994d4033f4a161a6d7078fd11595582f1
maary <24504742+Steve-Mr@users.noreply.github.com>
2023-01-19 22:58:19 +0800

    add history activity(buggy) and qs tile(half finished)

---
+++ app/src/main/java/com/maary/shareas/HistoryActivity.kt
@@ -0,0 +1,233 @@
+package com.maary.shareas
+
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.Settings
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.maary.shareas.databinding.ActivityHistoryBinding
+
+
+/** The request code for requesting [Manifest.permission.READ_EXTERNAL_STORAGE] permission. */
+private const val READ_EXTERNAL_STORAGE_REQUEST = 0x1045
+
+/**
+ * Code used with [IntentSender] to request user permission to delete an image with scoped storage.
+ */
+private const val DELETE_PERMISSION_REQUEST = 0x1033
+
+class HistoryActivity : AppCompatActivity(){
+
+    private val viewModel: HistoryActivityViewModel by viewModels()
+    private lateinit var binding: ActivityHistoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_history)
+
+        val galleryAdapter = GalleryAdapter { image ->
+            deleteImage(image)
+        }
+
+        binding.gallery.also { view ->
+            view.layoutManager = GridLayoutManager(this, 2)
+            view.adapter = galleryAdapter
+        }
+
+        viewModel.images.observe(this, Observer<List<MediaStoreImage>> { images ->
+            galleryAdapter.submitList(images)
+        })
+
+        viewModel.permissionNeededForDelete.observe(this, Observer { intentSender ->
+            intentSender?.let {
+                // On Android 10+, if the app doesn't have permission to modify
+                // or delete an item, it returns an `IntentSender` that we can
+                // use here to prompt the user to grant permission to delete (or modify)
+                // the image.
+                startIntentSenderForResult(
+                    intentSender,
+                    DELETE_PERMISSION_REQUEST,
+                    null,
+                    0,
+                    0,
+                    0,
+                    null
+                )
+            }
+        })
+
+        if (!haveStoragePermission()) {
+//            binding.welcomeView.visibility = View.VISIBLE
+            requestPermission()
+        } else {
+            showImages()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showImages()
+                } else {
+                    // If we weren't granted the permission, check to see if we should show
+                    // rationale for the permission.
+                    val showRationale =
+                        ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+
+                    /**
+                     * If we should show the rationale for requesting storage permission, then
+                     * we'll show [ActivityMainBinding.permissionRationaleView] which does this.
+                     *
+                     * If `showRationale` is false, this means the user has not only denied
+                     * the permission, but they've clicked "Don't ask again". In this case
+                     * we send the user to the settings page for the app so they can grant
+                     * the permission (Yay!) or uninstall the app.
+                     */
+                    if (showRationale) {
+//                        showNoAccess()
+                        //TODO: do something
+                    } else {
+                        goToSettings()
+                    }
+                }
+                return
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == DELETE_PERMISSION_REQUEST) {
+            viewModel.deletePendingImage()
+        }
+    }
+
+    private fun showImages() {
+        viewModel.loadImages()
+    }
+
+    private fun openMediaStore() {
+        if (haveStoragePermission()) {
+            showImages()
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun goToSettings() {
+        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName")).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }.also { intent ->
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Convenience method to check if [Manifest.permission.READ_EXTERNAL_STORAGE] permission
+     * has been granted to the app.
+     */
+    private fun haveStoragePermission() =
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    /**
+     * Convenience method to request [Manifest.permission.READ_EXTERNAL_STORAGE] permission.
+     */
+    private fun requestPermission() {
+        if (!haveStoragePermission()) {
+            val permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(this, permissions, READ_EXTERNAL_STORAGE_REQUEST)
+        }
+    }
+
+    private fun deleteImage(image: MediaStoreImage) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.delete_dialog_title)
+            .setMessage(getString(R.string.delete_dialog_message, image.displayName))
+            .setPositiveButton(R.string.delete_dialog_positive) { _: DialogInterface, _: Int ->
+                viewModel.deleteImage(image)
+            }
+            .setNegativeButton(R.string.delete_dialog_negative) { dialog: DialogInterface, _: Int ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    /**
+     * A [ListAdapter] for [MediaStoreImage]s.
+     */
+    private inner class GalleryAdapter(val onClick: (MediaStoreImage) -> Unit) :
+        ListAdapter<MediaStoreImage, ImageViewHolder>(MediaStoreImage.DiffCallback) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.gallery_layout, parent, false)
+            return ImageViewHolder(view, onClick)
+        }
+
+        override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+            val mediaStoreImage = getItem(position)
+            holder.rootView.tag = mediaStoreImage
+
+            Glide.with(holder.imageView)
+                .load(mediaStoreImage.contentUri)
+                .thumbnail(0.5f)
+                .centerCrop()
+                .into(holder.imageView)
+        }
+    }
+}
+
+/**
+ * Basic [RecyclerView.ViewHolder] for our gallery.
+ */
+private class ImageViewHolder(view: View, onClick: (MediaStoreImage) -> Unit) :
+    RecyclerView.ViewHolder(view) {
+    val rootView = view
+    val imageView: ImageView = view.findViewById(R.id.image)
+
+    init {
+        imageView.setOnClickListener {
+            val image = rootView.tag as? MediaStoreImage ?: return@setOnClickListener
+            onClick(image)
+        }
+    }
+}
