666126cde7583ef8997c62db8421426d89f90efd
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-04-19 22:20:59 +0800

    bump versionName

--- app/src/main/java/com/maary/shareas/fragment/EditorFragment.kt
+++ app/src/main/java/com/maary/shareas/fragment/EditorFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.addCallback
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
